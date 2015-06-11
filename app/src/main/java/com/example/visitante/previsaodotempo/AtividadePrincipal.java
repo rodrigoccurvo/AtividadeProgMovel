0423b77a48a481bff062099179487a86b9bfa4b8
Rodrigo <rodrigoccurvo@gmail.com>
2015-06-11 11:43:13 -0400

    Tarefa 2 completa

--- app/src/main/java/com/example/visitante/previsaodotempo/AtividadePrincipal.java
+++ app/src/main/java/com/example/visitante/previsaodotempo/AtividadePrincipal.java
@@ -4,16 +4,49 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 @SuppressWarnings("deprecation")
 public class AtividadePrincipal extends ActionBarActivity {
 
+    private ListAdapter mAdaptador;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atividade_principal);
+
+        // Cria dados falsos
+        String[] dados = {
+                "Segunda - Chuva",
+                "Terça - Mais Chuva",
+                "Quarta - Meio sol",
+                "Quinta - Sol",
+                "Sexta - Mais Sol",
+                "Sábado - Frigideira",
+                "Domingo - Torramos"
+        };
+
+        // Transforma dados em uma lista
+        List<String> previsao = new ArrayList<>( Arrays.asList(dados) );
+
+        // Cria o adaptador
+        mAdaptador = new ArrayAdapter<>(
+                getApplicationContext(), // Contexto atual
+                R.layout.item_lista_principal, // Nome do ID do layout
+                R.id.item_texto, // ID do TextView a ser preenchido
+                previsao); // Quais sao os dados
+
+        // Conecta a lista ao adaptador
+        ListView listView = (ListView) findViewById(R.id.lista_principal);
+        listView.setAdapter(mAdaptador);
     }
 
     @Override
